37c3adea5a84c85a3d1835006c7425b215f22c39
Srushti Milind Chavan <srushti.m.chavan@gmail.com>
2025-11-21 21:34:17 +0530

    Add files via upload
    LAPTOP.JAVA MADE USING BYTE DATA TYPE AS A EXAPMLE

---
+++ Laptop.java
@@ -0,0 +1,26 @@
+class Laptop
+{
+public static void main(String [] args)
+{
+ String brand="Lenovo";
+  String model="idea pad";
+   byte ram=16;
+  byte keys=70;
+ byte ports=6;
+ byte speakers=4;
+ byte generation=11;
+ byte graphicCard=4;
+byte core=10;
+ System.out.println("-----LAPTOP DETAILS-----");
+System.out.println("model : "+model);
+System.out.println("brand :  "+brand);
+System.out.println("ram : "+ram+"gb");
+System.out.println("keys : "+keys);
+System.out.println("ports : "+ports);
+System.out.println("no of speakers : "+speakers);
+System.out.println("genegration :I"+generation+"th");
+System.out.println("no of graphic Cards : "+graphicCard);
+System.out.println("core : "+core);
+}
+}
+ 
